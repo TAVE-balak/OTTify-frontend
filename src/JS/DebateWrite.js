c372cfab13bc206905c0b5c981dda440923cf12c
wonhyuna <hyunawon417@gmail.com>
2024-01-03 17:20:14 +0900

    Feat: 토론 쓰기 내용에 따른 버튼 색상 변경 (#30)

--- src/JS/DebateWrite.js
+++ src/JS/DebateWrite.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import back from '../img/back.png';
@@ -10,6 +11,18 @@ const DebateWrite = () => {
   const goToDebateOne = () => {
     navigate('/DebateOne');
   }
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleContentChange = (event) => {
+    setContent(event.target.value);
+  };
+  
   return (
     <div className="debateWrite">
       <div className='debateAuto'>
@@ -19,17 +32,25 @@ const DebateWrite = () => {
         </div>
         <div className ="writeMain">
           <div className='writeTitle'>
-            <input className='titleArea' placeholder={"제목"}></input>
+            <input className='titleArea' 
+                    placeholder="제목"
+                    value = {title}
+                    onChange={handleTitleChange}>        
+            </input>
           </div>
           <div className='writeContent'>
-            <textarea className='contentArea' placeholder={"작품에 대한 감상을 다른 분들과 공유해 보세요."}></textarea>
+            <textarea className='contentArea' 
+                      placeholder="작품에 대한 감상을 다른 분들과 공유해 보세요."
+                      value = {content}
+                      onChange={handleContentChange}>          
+            </textarea>
           </div>
           <div className='writeImg'>
             <img src = {photo_select} className='photo_select'></img>
             <span className='photo_name'>사진 추가</span>
           </div>
           <div className='writeBtn'>
-            <button className = "completeBtn">작성 완료</button>
+            <button className = {`completeBtn ${title && content ? 'active' : ''}`}>작성 완료</button>
           </div>
         </div>
       </div>
