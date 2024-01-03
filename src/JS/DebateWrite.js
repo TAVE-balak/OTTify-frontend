5536b6f6c6593a77d7b7c41babefccc421ea7aaa
wonhyuna <hyunawon417@gmail.com>
2024-01-03 17:10:53 +0900

    Feat: 토론 쓰기 배치 UI (#30)

--- src/JS/DebateWrite.js
+++ src/JS/DebateWrite.js
@@ -6,23 +6,25 @@ import '../CSS/DebateWrite.css';
 const DebateWrite = () => {
   return (
     <div className="debateWrite">
-      <div className="writeTop">
-        <img src = {back} className='writeBack'></img>
-        <span class = "topTitle">토론 열기</span>
-      </div>
-      <div className ="writeMain">
-        <div className='writeTitle'>
-          <input className='titleArea' value={"제목"}></input>
+      <div className='debateAuto'>
+        <div className="writeTop">
+          <img src = {back} className='writeBack'></img>
+          <span class = "topTitle">토론 열기</span>
         </div>
-        <div className='writeContent'>
-          <input className='contentArea' value={"작품에 대한 감상을 다른 분들과 공유해 보세요."}></input>
-        </div>
-        <div className='writeImg'>
-          <img src = {photo_select} className='photo_select'></img>
-          <span className='photo_name'>사진 추가</span>
-        </div>
-        <div className='writeBtn'>
-          <button className = "completeBtn">작성 완료</button>
+        <div className ="writeMain">
+          <div className='writeTitle'>
+            <input className='titleArea' placeholder={"제목"}></input>
+          </div>
+          <div className='writeContent'>
+            <textarea className='contentArea' placeholder={"작품에 대한 감상을 다른 분들과 공유해 보세요."}></textarea>
+          </div>
+          <div className='writeImg'>
+            <img src = {photo_select} className='photo_select'></img>
+            <span className='photo_name'>사진 추가</span>
+          </div>
+          <div className='writeBtn'>
+            <button className = "completeBtn">작성 완료</button>
+          </div>
         </div>
       </div>
     </div>
