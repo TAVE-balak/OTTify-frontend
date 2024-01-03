f52af80cea759111b797131eadd4f9196e08cc45
wonhyuna <hyunawon417@gmail.com>
2024-01-03 15:54:27 +0900

    Feat: 토론 쓰기 구조 배치 완료 (#30)

--- src/JS/DebateWrite.js
+++ src/JS/DebateWrite.js
@@ -1,5 +1,30 @@
-const DebateWrite = () => {
+import back from '../img/back.png';
+import photo_select from '../img/photo_select.png';
 
+const DebateWrite = () => {
+  return (
+    <div className="debateWrite">
+      <div className="writeTop">
+        <img src = {back} className='writeBack'></img>
+        <span class = "topTitle">토론 열기</span>
+      </div>
+      <div className ="writeMain">
+        <div className='writeTitle'>
+          <textarea className='titleArea'></textarea>
+        </div>
+        <div className='writeContent'>
+          <textarea className='contentArea'></textarea>
+        </div>
+        <div className='writeImg'>
+          <img src = {photo_select} className='photo_select'></img>
+          <span className='photo_name'>사진 추가</span>
+        </div>
+        <div className='writeBtn'>
+          <button classname = "completeBtn">작성 완료</button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default DebateWrite;
