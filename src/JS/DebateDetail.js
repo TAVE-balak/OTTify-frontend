9afca49f6a295c2872b3214a60d5544fb3130975
wonhyuna <hyunawon417@gmail.com>
2024-01-05 02:27:46 +0900

    Feat: 토론 상세 페이지 - 댓글 달기 (#37)

--- src/JS/DebateDetail.js
+++ src/JS/DebateDetail.js
@@ -1,3 +1,4 @@
+import {useRef, useCallback} from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import '../CSS/MyDebate.css';
@@ -8,15 +9,29 @@ import back from '../img/back.png';
 
 const DebateDetail = () =>{
   const navigate = useNavigate();
- 
+
+  const textRef = useRef();
+  const handleResizeHeight = useCallback(() => {
+    textRef.current.style.height = textRef.current.scrollHeight + "px";
+  }, []);
+  
+
   return (
     <div className='DebateDetail'>
       <div className = "debatedetail_page">
         <div className = "debatedetail_title">
-          <img src = {back} className = "debatedetail_back" alt = "뒤로 가기" onClick={() => navigate(-1)}/>
+          <img src = {back} id="debateCommentTextarea" className = "debatedetail_back" alt = "뒤로 가기" onClick={() => navigate(-1)}/>
           <h2>토론 상세 보기</h2>
         </div>
-        <DetailList/>
+        
+        <div className='debatedetail_Main'>
+          <DetailList/>
+        </div>
+
+        <div className='debatedetail_comment'>
+          <textarea ref = {textRef} className='debate_comment' placeholder='댓글을 작성해주세요' onInput = {handleResizeHeight}></textarea>
+          <button className='comment_enroll'>등록</button>
+        </div>
       </div>
     </div>
   )
