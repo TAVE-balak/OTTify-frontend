21eee711da6f7b3acbaac10f52e9c5ea22240521
wonhyuna <hyunawon417@gmail.com>
2024-01-06 02:59:37 +0900

    Feat: 토론 상세 페이지 - 댓글 세부 UI 완료 (#37)

--- src/JS/CommentItem.js
+++ src/JS/CommentItem.js
@@ -7,10 +7,14 @@ const CommentItem = ({author, content, favorite, profile, created_date, id}) =>{
   return (
     <div className = "CommentItem">
       <div className = "comments_info">
-        <img src = {profile} className="comment_profile"></img>
-        <span className="comment_author">{author}님</span>
-        <span className="comment_date">{created_date}</span>
-        <img src = {more} className ="comment_more"></img>
+        <div>
+          <img src = {profile} className="comment_profile"></img>
+          <span className="comment_author">{author}님</span>
+        </div>
+        <div>
+          <span className="comment_date">{created_date}</span>
+          <img src = {more} className ="comment_more"></img>
+        </div>
       </div>
 
       <div className = "comments_content">
