f24943d8e965d4243b5e36c884aacb6b332228ae
wonhyuna <hyunawon417@gmail.com>
2024-01-07 22:58:44 +0900

    Feat: 토론 상세 페이지 - 대댓글 작성 UI (#37)

--- src/JS/CommentReplyItem.js
+++ src/JS/CommentReplyItem.js
@@ -1,12 +1,31 @@
+import commentreply_img from '../img/second_comment_vector.png';
+import more from '../img/more.png';
+import thumb from '../img/thumb_up.png';
+
 const CommentReplyItem = ({author, content, favorite, created_date, id}) =>{
   
 
   return (
     <div className = "CommentReplyItem">
-      <div>{author}</div>
-      <div>{content}</div>
-      <div>{favorite}</div>
-      <div>{created_date}</div>
+      <img src = {commentreply_img} className='commentreply_img'></img>
+      <div className='commentreply_all'>
+        <div className='commentreply_info'>
+          <span className='commentreply_author'>{author}님</span>
+          <div className='commentreply_right'>
+            <span className='commentreply_date'>{created_date}</span>
+            <img src = {more} className='comment_more'></img>
+          </div>
+        </div>
+
+        <div className='commentreply_content'>
+          {content}
+        </div>
+
+        <div className='commentreply_reaction'>
+          <img src = {thumb} className='comment_thumb'></img>
+          <span className='commentreply_favoriteNum'>{favorite}</span>
+        </div>
+      </div>
 
     </div>
   )
