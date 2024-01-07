73fd553d14d446ab55e31651eb2d35fb4a496294
wonhyuna <hyunawon417@gmail.com>
2024-01-07 23:03:44 +0900

    Feat: 토론 상세 페이지 - 대댓글 메뉴 띄우기 (#37)

--- src/JS/CommentReplyItem.js
+++ src/JS/CommentReplyItem.js
@@ -1,9 +1,14 @@
+import {useState} from 'react';
 import commentreply_img from '../img/second_comment_vector.png';
 import more from '../img/more.png';
 import thumb from '../img/thumb_up.png';
 
 const CommentReplyItem = ({author, content, favorite, created_date, id}) =>{
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <div className = "CommentReplyItem">
@@ -13,7 +18,13 @@ const CommentReplyItem = ({author, content, favorite, created_date, id}) =>{
           <span className='commentreply_author'>{author}님</span>
           <div className='commentreply_right'>
             <span className='commentreply_date'>{created_date}</span>
-            <img src = {more} className='comment_more'></img>
+            <img src = {more} className='comment_more' onClick={handleMenuClick}></img>
+            {isMenuOpen && (
+              <div className='menu_list'>
+                <div className='menu_edit'>댓글 수정</div>
+                <div className='menu_delete'>댓글 삭제</div>
+              </div>
+            )}
           </div>
         </div>
 
