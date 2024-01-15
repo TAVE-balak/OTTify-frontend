19358fe0c99a55c3de31636086a4223face9dafd
wonhyuna <hyunawon417@gmail.com>
2024-01-15 17:57:33 +0900

    Feat: 마이페이지 API 연결 (#53)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import '../CSS/Nav.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ottify from '../img/ottify.png';
 import profile from '../img/사진.jpg';
 import search from '../img/search.png';
 import Login from "./Login/Login";
+//api 호출
+import {fetchUserProfile} from '../JS/WonAPI';
 
 const Nav = () => {
   const [searchValue, setSearchValue] = useState('');
@@ -33,6 +35,24 @@ const Nav = () => {
     setIsLoginModalOpen(!isLoginModalOpen); // 로그인 모달 가시성 토글
   };
 
+  //api 호출 (마이페이지)
+  const userId = 1;
+  const fetchData = async () => {
+    try {
+      await fetchUserProfile(userId);
+    } catch (error) {
+      console.error('Error fetching data from API:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(); // 마운트 시에 데이터를 불러오기 위한 호출
+  }, []);
+
+  const fetchDataOnClick = async () => {
+    fetchData(); // 버튼 클릭 시에도 동일한 로직을 수행
+  };
+
   return (
     <>
       <ul className="navbar">
@@ -49,7 +69,7 @@ const Nav = () => {
           type="text"
           placeholder="영화를 검색해주세요."
         />
-        <li className="nav-item" onClick={goToMypage}>
+        <li className="nav-item" onClick={(e)=>{goToMypage(e); fetchDataOnClick(e)}}>
           <img src={profile} className='nav_profile' alt="User" />
         </li>
         {/* Add a login button */}
