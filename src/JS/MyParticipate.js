8d606b5b49798a1aef8199587ac9101f75da0261
wonhyuna <hyunawon417@gmail.com>
2024-01-17 13:53:38 +0900

    Feat: 내가 참여한 토론 모아보기 api 연결 (#53)

--- src/JS/MyParticipate.js
+++ src/JS/MyParticipate.js
@@ -1,50 +1,71 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import {useEffect, useState} from 'react';
 import DebateList from './DebateList';
+import { fetchMyParticipate } from "./WonAPI";
 import '../CSS/MyDebate.css'
 
 import back from '../img/back.png';
 import poster from '../img/debate_poster.png';
 
 const MyParticipate = () =>{
+  const {userId} = useParams();
+  const [myFavoriteData, setMyFavoriteData] = useState(null);
   const navigate = useNavigate();
 
-  const dummyList = [
-  {
-    id: 1,
-    author: "김영리", 
-    movie: "나폴레옹",
-    poster: "",
-    debateTitle: "서울의 봄은 최고의 영화이다",
-    content: "봉준호 영화 중에서 가장 음침하고 불편한 영화인 것 같다. <마더>에 가득 찬 오해들은 풀리지 못 한다. 오해를 오해로 해결하더니 끝내는 엉뚱한 사람이 갇힌다. 이 영화에서 해결된 것은 없다. 잊으려 애를 쓸 뿐이다. 하지만 그것도 불안하기 짝이 없어 보인다. 가나다라마바사 아자차카파타하.",
-    created_date: "3달 전",
-    favorite: 120,
-    comment: 4,
-  }, 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let storedMyFavorite = sessionStorage.getItem(`myFavoriteData_${userId}`);
+        let MyDiscussionFavoriteData;
 
-  {
-    id: 2,
-    author: "김영리", 
-    movie: "나폴레옹",
-    poster: {poster},
-    debateTitle: "서울의 봄은 최고의 영화이다",
-    content: "",
-    created_date: "3달 전",
-    favorite: 120,
-    comment: 4,
-  }, 
+        if (storedMyFavorite) {
+          // 세션 스토리지에 사용자 정보가 있으면 가져오기
+          MyDiscussionFavoriteData = JSON.parse(storedMyFavorite);
+        } else {
+          // 세션 스토리지에 사용자 정보가 없으면 API 호출하여 가져오기
+          MyDiscussionFavoriteData = await fetchMyParticipate(userId);
+          // 가져온 정보를 세션 스토리지에 저장
+          sessionStorage.setItem(`myFavoriteData_${userId}`, JSON.stringify(MyDiscussionFavoriteData));
+        }
+        setMyFavoriteData(MyDiscussionFavoriteData);
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    };
+    if (userId) {
+      fetchData();
+    }
+  }, [userId]);
 
-  {
-    id: 3,
-    author: "김영리", 
-    movie: "나폴레옹",
-    poster: {poster},
-    debateTitle: "서울의 봄은 최고의 영화이다",
-    content: "봉준호 영화 중에서 가장 음침하고 불편한 영화인 것 같다. <마더>에 가득 찬 오해들은 풀리지 못 한다. 오해를 오해로 해결하더니 끝내는 엉뚱한 사람이 갇힌다. 이 영화에서 해결된 것은 없다. 잊으려 애를 쓸 뿐이다. 하지만 그것도 불안하기 짝이 없어 보인다. 가나다라마바사 아자차카파타하.",
-    created_date: "3달 전",
-    favorite: 120,
-    comment: 4,
-  }
-]
+  const dummyList = myFavoriteData ? myFavoriteData.data.map(item => {
+    const targetDate = new Date(item.createdDate);
+    const currentDate = new Date();
+    const timeDiff = currentDate - targetDate;
+    // 밀리초를 일로 변환
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  
+    let displayDate;
+    if (daysDiff < 30) {
+      displayDate = `${daysDiff}일 전`;
+    } else if (daysDiff < 365) {
+      const monthsDiff = Math.floor(daysDiff / 30);
+      displayDate = `${monthsDiff}달 전`;
+    } else {
+      const yearsDiff = Math.floor(daysDiff / 365);
+      displayDate = `${yearsDiff}년 전`;
+    }
+  
+    return {
+      id: item.discussonId,
+      movie: item.programTitle,
+      poster: item.imgUrl || "",
+      debateTitle: item.discussionTitle,
+      content: item.content,
+      created_date: displayDate,
+      favorite: item.likeCnt,
+      comment: item.replyCnt
+    };
+  }) : [];
 
 
   return (
