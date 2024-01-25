d90ef96668ef8d4e0b9211b5eb5bb92e3379113c
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-26 04:49:23 +0900

    Feature: OTT 정보 API 연동 (#79)

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -22,8 +22,7 @@ function MovieModal({ setModalOpen, ...movie }) {
   const [wishSrc, setWishSrc] = useState(likeImage); // 초기 상태는 선택이 되지 않은 상태를 나타내기 위함
   const [isNotInterested, setIsNotInterested] = useState(false); // 관심없음
   const [notInterestedSrc, setNotInterestedSrc] = useState(notInterestedImage);
-
-  const [activePriceType, setActivePriceType] = useState(null);
+  const [activePriceType, setActivePriceType] = useState("streaming"); // streaming, rent, buy
 
   const handleHeart = () => {
     if (isWish) {
@@ -34,29 +33,6 @@ function MovieModal({ setModalOpen, ...movie }) {
       setIsWish(!isWish);
     }
   };
-
-  const handleButtonClick = (priceType) => {
-    // 가격 유형 활성화 (정액제, 대여, 구매)
-    setActivePriceType(priceType);
-  };
-
-  const services = [
-    {
-      name: "넷플릭스",
-      link: "https://help.netflix.com/ko/node/24926",
-      subscriptionPrice: "8,900원",
-      rentalPrice: null, // 만약 해당 서비스에 대여 가격이 없으면 null 이나 적절한 값을 넣으세요.
-      buyPrice: null, // 만약 해당 서비스에 구매 가격이 없으면 null 이나 적절한 값을 넣으세요.
-    },
-    {
-      name: "왓차",
-      link: "https://help.watcha.co.kr/hc/ko/articles/900003415306-%EC%99%93%EC%B1%A0%EC%97%90%EC%84%9C-%ED%8C%90%EB%A7%A4-%EC%A4%91%EC%9D%B8-%EC%9D%B4%EC%9A%A9%EA%B6%8C-%EC%A2%85%EB%A5%98%EA%B0%80-%EA%B6%81%EA%B8%88%ED%95%B4%EC%9A%94-",
-      subscriptionPrice: "8,900원",
-      rentalPrice: "12,000원", // 만약 해당 서비스에 대여 가격이 없으면 null 이나 적절한 값을 넣으세요.
-      buyPrice: null, // 만약 해당 서비스에 구매 가격이 없으면 null 이나 적절한 값을 넣으세요.
-    },
-    // 왓차, 티빙 등 다른 서비스 정보를 여기에 추가...
-  ];
 
   const handleDiscuss = () => {
     // 토론 관련 로직
@@ -90,7 +66,7 @@ function MovieModal({ setModalOpen, ...movie }) {
   const [programDetail, setProgramDetail] = useState();
   const [programNormalReviewRating, setProgramNormalReviewRating] =
     useState(0.0);
-  const [programProviderList, setProgramProviderList] = useState([]);
+  const [programProviders, setProgramProviders] = useState([]);
 
   useEffect(() => {
     // 가정: API 호출로 캐스트 정보를 가져오는 함수가 있음
@@ -103,7 +79,7 @@ function MovieModal({ setModalOpen, ...movie }) {
         setCast(data.oaProgramCreditsDto.cast); // 받아온 데이터로 상태 업데이트
         setProgramDetail(data.programDetailResponse);
         setProgramNormalReviewRating(data.programNormalReviewRating);
-        setProgramProviderList(data.programProviderListResponseDto);
+        setProgramProviders(data.programProviderListResponseDto);
       } catch (error) {
         console.error("Fetching cast failed", error);
       }
@@ -111,6 +87,39 @@ function MovieModal({ setModalOpen, ...movie }) {
 
     fetchCast();
   }); // movieId가 변하지 않는 이상 호출되지 않음
+
+  const handleButtonClick = (priceType) => {
+    // 가격 유형 활성화 (정액제, 대여, 구매)
+    setActivePriceType(priceType);
+  };
+
+  const renderProviderSection = (activePriceType) => {
+    let services;
+    switch (activePriceType) {
+      case "streaming":
+        services = programProviders.streaming;
+        console.log(2, services);
+        break;
+      case "rent":
+        services = programProviders.rent;
+        break;
+      case "buy":
+        services = programProviders.buy;
+      default:
+    }
+
+    if (services && services.length > 0) {
+      return services.map((service, index) => (
+        <div className="service-box" key={index}>
+          <img
+            src={`https://image.tmdb.org/t/p/original/${service.logo_path}`}
+            style={{ borderRadius: "20px" }}
+          />
+          {service.provider_name}
+        </div>
+      ));
+    }
+  };
 
   return (
     <div className="presentation">
@@ -227,50 +236,19 @@ function MovieModal({ setModalOpen, ...movie }) {
             </div>
 
             {/* OTT 정보 테이블 */}
-            <table>
-              <thead>
-                <h2>보러가기</h2>
-                <tr>
-                  <th>OTT</th>
-                  <th>정액제</th>
-                  <th>대여</th>
-                  <th>구매</th>
-                  <th>가격</th>
-                </tr>
-              </thead>
-              <tbody>
-                {services.map((service, index) => (
-                  <tr key={index}>
-                    <td>
-                      <div className="service-box">
-                        <a
-                          href={service.link}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                        >
-                          {service.name}
-                        </a>
-                      </div>
-                    </td>
-                    <td>
-                      <button onClick={() => handleButtonClick("subscription")}>
-                        정액제
-                      </button>
-                    </td>
-                    <td>
-                      <button onClick={() => handleButtonClick("rental")}>
-                        대여
-                      </button>
-                    </td>
-                    <td>
-                      <button onClick={() => handleButtonClick("buy")}>
-                        구매
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <div>
+              <h2>보러가기</h2>
+              <button onClick={() => handleButtonClick("streaming")}>
+                스트리밍 <span>{programProviders.streamingSize}</span>
+              </button>
+              <button onClick={() => handleButtonClick("rent")}>
+                대여 <span>{programProviders.rentSize}</span>
+              </button>
+              <button onClick={() => handleButtonClick("buy")}>
+                구매 <span>{programProviders.buySize}</span>
+              </button>
+              {renderProviderSection(activePriceType)}
+            </div>
 
             <h2>출연/제작</h2>
             <Casts castList={cast} />
