ea02d9fe4b70f0b925a919f03a5da46926ed4166
wonhyuna <hyunawon417@gmail.com>
2024-02-16 08:10:04 +0900

    Refactor: 마이페이지 내가 쓴 리뷰 api 연결 완료 (#101)

--- src/JS/WonAPI.js
+++ src/JS/WonAPI.js
@@ -9,8 +9,8 @@ const getAccessToken = () => {
 };
 
 const fetchUserProfile = async () => {
-  const accessToken = getAccessToken();
-  // const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcwNzkxNzc0NywiZW1haWwiOiJoeXVuYXdvbjQxN0BnbWFpbC5jb20ifQ.H4EN_7R-LMB5EgutIcNCEc38Q7D--TkXsWG2FfpLcbXM_LQ3TQ0jq6_A3IHTnfPHH8uOfltKU-Cgo7v5pDkDKw"
+  // const accessToken = getAccessToken();
+  const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcwODA0MTQyNCwiZW1haWwiOiJoeXVuYXdvbjQxN0BnbWFpbC5jb20ifQ.Yxt6xAoHXvIoNdpGotmSP-uJVOxWRktSrqFIvR5PCxEaADYzvZHnzrWkTwq4fJP25_EsKN7rnTDazKyc_zKdiQ"
   try {
     const response = await Wonapi.get(`/api/v1/users/`,{
       headers: {
@@ -24,8 +24,8 @@ const fetchUserProfile = async () => {
 };
 
 const fetchMyWrite = async () => {
-  const accessToken = getAccessToken();
-  // const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGci/OiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcwNzkxNzc0NywiZW1haWwiOiJoeXVuYXdvbjQxN0BnbWFpbC5jb20ifQ.H4EN_7R-LMB5EgutIcNCEc38Q7D--TkXsWG2FfpLcbXM_LQ3TQ0jq6_A3IHTnfPHH8uOfltKU-Cgo7v5pDkDKw"
+  // const accessToken = getAccessToken();
+  const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBY2Nlc3NUb2tlbiIsImV4cCI6MTcwODA0MTQyNCwiZW1haWwiOiJoeXVuYXdvbjQxN0BnbWFpbC5jb20ifQ.Yxt6xAoHXvIoNdpGotmSP-uJVOxWRktSrqFIvR5PCxEaADYzvZHnzrWkTwq4fJP25_EsKN7rnTDazKyc_zKdiQ"
   try {
     const response = await Wonapi.get(`/api/v1/users/reviews`,{
       headers: {
