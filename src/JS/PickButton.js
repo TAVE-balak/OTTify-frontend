27ebf41174b86c66a85e7851f6d7f4ee0fd0790f
wonhyuna <hyunawon417@gmail.com>
2024-02-25 15:16:41 +0900

    Refactor: 마이페이지 2순위 취향 장르 api 연결 수정 (#118)

--- src/JS/PickButton.js
+++ src/JS/PickButton.js
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { update2ndGenre } from './WonAPI';
 
 function PickButton({ genre, userId, userProfileGenres, children }) {
+  //2순위 장르 세션 스토리지 추가
+  const [sessionData, setSessionData] = useState([]);
+
+  useEffect(() => {
+    const data = JSON.parse(sessionStorage.getItem('userProfileGenres'));
+    if (data) {
+      setSessionData(data);
+    } else {
+      // 세션 스토리지에 데이터가 없을 경우 userProfileGenres의 값을 초기값으로 설정
+      const initialData = [];
+      setSessionData(initialData);
+      sessionStorage.setItem('userProfileGenres', JSON.stringify(initialData));
+    }
+  }, [userProfileGenres]);
+
+
   // 각 버튼의 독립적 상태 관리 위해 useState 이용
+  const data = JSON.parse(sessionStorage.getItem('userProfileGenres'));
   const [buttonState, setButtonState] = useState(
-    userProfileGenres.some(userGenre => userGenre.name === genre.name)
+    data.some(userGenre => userGenre === genre.name)
   );
   const [backgroundColor, setBackgroudColor] = useState(
     buttonState ? '#ffA500' : '#EEEEEE'
   );
   const [textColor, setTextColor] = useState(buttonState ? '#FFFFFF' : '#000000');
 
+
   // 버튼 클릭 이벤트 핸들러 함수
   const handleBtnClick = async () => {
     try {
       const newBackgroundColor = buttonState ? '#EEEEEE' : '#ffA500';
       const newTextColor = buttonState ? '#000000' : '#FFFFFF';
-
+      
       setButtonState(!buttonState);
       setBackgroudColor(newBackgroundColor);
       setTextColor(newTextColor);
@@ -25,8 +43,16 @@ function PickButton({ genre, userId, userProfileGenres, children }) {
       const updatedRequestDto = {
         genreId: genre.id,
       };
-      const updatedData = await update2ndGenre(updatedRequestDto, userId);
+      const updatedData = await update2ndGenre(updatedRequestDto);
       console.log("2ndGenre가 성공적으로 업데이트되었습니다:", updatedData);
+
+      // 세션 스토리지 업데이트
+      const newData = sessionData.includes(genre.name)
+        ? sessionData.filter(item => item !== genre.name)
+        : [...sessionData, genre.name];
+      setSessionData(newData);
+      sessionStorage.setItem('userProfileGenres', JSON.stringify(newData));
+      
     } catch (error) {
       console.error("2ndGenre 업데이트 중 에러 발생:", error);
     }
